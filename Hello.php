832ee5382d638c5b31296f6cb4615f11aa504466
SMuhammed <kgz.muhammed@gmail.com>
2017-03-16 17:03:48 +0600

    Hello world friend

---
+++ Hello.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+
+<?PHP
+	echo "Hello, world!";
+?>
+<br />
+<br />
+<br />
+<br />
+<div id="menuHrefs">
+          <a  href=" D:\OpenServer\domains\MyTravelNotes\labs.php" title="На главную"  id="logo">laba1</a>
+          
+         </div>
+
+
+</body>
+</html>
